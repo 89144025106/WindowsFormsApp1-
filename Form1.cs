549d8f042af06c56a4667de3db2d9f1a86796dea
Кристина Гуляева <kristinagul.24@gmail.com>
2022-09-22 16:05:59 +1000

    добавила кнопку

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1_Гуляева_Кристина
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Helloy";
+        }
     }
 }
